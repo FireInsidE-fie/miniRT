9b188e4a3754876ce5544b143f1523c097bb0b7c
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-06-06 09:17:15 +0200

    Add mlx hook for pressing the `ESC` key

--- src/mlx.c
+++ src/mlx.c
@@ -1,5 +1,8 @@
 #include "minirt.h"
+#include <X11/X.h>
 #include "mlx.h"
+
+#define KEY_ESC 65307
 
 t_core	*get_core(void)
 {
@@ -10,12 +13,25 @@ t_core	*get_core(void)
 	return (&core);
 }
 
+static int	key_press(int key, void *param)
+{
+	t_core	*core;
+
+	core = param;
+	if (key == KEY_ESC)
+		return (rt_kill(core));
+	return (0);
+}
+
+/**
+ * @brief Creates hooks for the minilibX, quitting the program when the main
+ * window is destroyed or the `ESC` key is pressed.
+ */
 static void	init_hooks(t_core *core)
 {
 	core = get_core();
-	// TODO: Hook for pressing `q` ending program
-	// mlx_hook(core->win, 2, 0, key_press, core);
-	mlx_hook(core->win, 17, 0, rt_kill, core);
+	mlx_hook(core->win, KeyPress, KeyPressMask, key_press, core);
+	mlx_hook(core->win, DestroyNotify, 0, rt_kill, core);
 }
 
 /**
