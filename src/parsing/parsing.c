ef57421bc0806d30dfbd70cb05bf6bb914762287
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-08-08 15:25:36 +0200

    add two scenes and prepare parsing file structure

---
+++ src/parsing/parsing.c
@@ -0,0 +1,18 @@
+#include "minirt.h"
+
+/**
+ * @brief Parses a given .rt file and creates the scene from it.
+ *
+ * @details Error messages:
+ * 0 - All went well
+ * 1 - Path is not a .rt file
+ * 2 -
+ */
+int	parse_scene(char *scene_path)
+{
+	t_scene	*scene;
+
+	scene = &get_core()->scene;
+
+	return (0);
+}
