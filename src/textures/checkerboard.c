7ab19c8c451854a475d80a59514bce80d6c6d6ba
0yech <Nelson.rey02@gmail.com>
2025-09-22 17:06:12 +0200

    Added comments to checkerboards

--- src/textures/checkerboard.c
+++ src/textures/checkerboard.c
@@ -12,8 +12,12 @@
 #include <stdbool.h>
 #include <math.h>
 
-// Swap out 10 for any other scale
-
+/*	Per pixel/light check, given an uv point, return
+*	a black t_color if the result is odd.
+*	the size of the checkerboard pattern can be changed.
+*	
+*	the size is the (* 10) in this case.
+*/
 t_color	apply_checker_sphere(t_shape *sphere, t_uv uv)
 {
 	int		x;
@@ -21,23 +25,32 @@ t_color	apply_checker_sphere(t_shape *sphere, t_uv uv)
 
 	x = (int)(uv.u * 10);
 	y = (int)(uv.v * 10);
-
 	if ((x + y) % 2 == 0)
 		return (sphere->mat.color); // t_color
 	else
-		return (t_color){0, 0, 0}; // Black
+		return ((t_color){0, 0, 0}); // Black
 }
 
+/*	Per pixel/light check, given an x and z point, return
+*	a black t_color if the result is odd.
+*	the size of the checkerboard pattern can be changed.
+*	
+*	The floor() function rounds a number DOWN to the nearest
+*	integer (but as double). -5.1 becomes -6.0 and 5.1 becomes 5.0
+*
+*	Since we are working with x and z coords, a vertical
+*	plane would look striped instead of checkered.
+*	the size is the (/ 10) in this case.
+*/
 t_color	apply_checker_plane(t_shape *plane, t_point3 *point)
 {
-	int x;
-	int y;
+	int	x;
+	int	y;
 
 	x = (int)floor(point->x / 10);
 	y = (int)floor(point->z / 10);
-
 	if ((x + y) % 2 == 0)
 		return (plane->mat.color); // t_color
 	else
-		return (t_color){0, 0, 0}; // Black
+		return ((t_color){0, 0, 0}); // Black
 }
