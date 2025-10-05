5c6c3ae44d925c778c30e833a5acb5d2ae039b52
0yech <Nelson.rey02@gmail.com>
2025-10-05 22:22:28 +0200

    added export support for triangles

--- src/export/export.c
+++ src/export/export.c
@@ -1,5 +1,24 @@
 #include "export.h"
+#include <stdlib.h>
 #include <fcntl.h>
+
+void	export_shapes(t_shape *shape, int fd)
+{
+	while (shape != NULL)
+	{
+		if (shape->type == SPHERE)
+			export_sphere(shape, fd);
+		else if (shape->type == PLANE)
+			export_plane(shape, fd);
+		else if (shape->type == CYLINDER)
+			export_cylinder(shape, fd);
+		else if (shape->type == CONE)
+			export_cone(shape, fd);
+		else if (shape->type == TRIANGLE)
+			export_triangle(shape, fd);
+		shape = shape->next;
+	}
+}
 
 void	export_scene(void)
 {
